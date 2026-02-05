9dcd5fe88f7cf6f254f23941f0efdcfcf674219c
Meetheora <xenotimedev@gmail.com>
2026-02-05 11:57:51 +0100

    Made Player.rb

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -1,7 +1,9 @@
 class Player
-  #TO DO : la classe a 2 attr_reader, son nom et sa valeur (X ou O).
+  attr_reader :name, :symbol
 
   
-  def initialize
-    #TO DO : doit régler son nom et sa valeur
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
   end
+end
