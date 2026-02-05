97bee052baa93b46f7a476cecc12c1bda32dfc5e
crousty24-bit <allen.kch24@gmail.com>
2026-02-05 11:15:52 +0100

    add file structure and methods

--- app.rb
+++ app.rb
@@ -3,4 +3,17 @@ Bundler.require
 
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'app/player'
-require 'views/show'
+require 'views/show' # voir pour la syntaxe !
+
+
+class Application
+  def perform
+    # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
+    # 
+
+  end
+
+end
+
+
+Application.new.perform
