38e0e70bb2a3469037606eb0ffd8ada92ebcc8ea
Meetheora <xenotimedev@gmail.com>
2026-02-05 16:41:17 +0100

    Finalized board.rb

--- lib/app/board.rb
+++ lib/app/board.rb
@@ -1,5 +1,5 @@
 class Board
-  attr_accessor :board_cases, :count_turn
+  attr_accessor :cases, :count_turn
 
   def initialize
     @cases = {} #Initializing board's hash
@@ -13,13 +13,17 @@ class Board
 
   def victory?
     @victory_scenarios = [
-    ["A1","A2","A3"], ["B1","B2","B3"], ["C1","C2","C3"]
-    ["A1","B1","C1"], ["A2","B2","C2"], ["A3","B3","C3"]
+    ["A1","A2","A3"], ["B1","B2","B3"], ["C1","C2","C3"],
+    ["A1","B1","C1"], ["A2","B2","C2"], ["A3","B3","C3"],
     ["A1","B2","C3"], ["A3","B2","C1"]
   ]
-
     victory_scenarios.each do |scenario|
-      #obtenir les 3 symboles ?
+      case1_state = @cases[scenario[0]].case_state
+      case2_state = @cases[scenario[1]].case_state
+      case3_state = @cases[scenario[2]].case_state
+    
+    if case1_state == case2_state && case1_state == case3_state && case1_state != " "
+    return case1_state
     end
 
     if count_turn ==  9
