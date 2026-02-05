6593ded6f5bea5e1537c1a8927b3a44e2baf1bc4
crousty24-bit <allen.kch24@gmail.com>
2026-02-05 12:12:35 +0100

    add class BoardCase & method initialize

--- lib/app/board_case.rb
+++ lib/app/board_case.rb
@@ -1,9 +1,8 @@
 class BoardCase
-  #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
+  attr_accessor :case_id, :case_state
 
-  
-  def initialize
-    #TO DO : doit régler sa valeur, ainsi que son numéro de case
+  def initialize(case_id, case_state)
+    @case_id = case_id
+    @case_state = case_state #should receive a "string"
   end
-  
 end
